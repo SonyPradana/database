da534d99d45cd80d479cd9e58d4b1430c05d09bb
SonyPradana <sonypradana@gmail.com>
2025-10-05 23:34:06 +0000

    v0.40.0

--- MyPDO.php
+++ MyPDO.php
@@ -4,19 +4,26 @@ declare(strict_types=1);
 
 namespace System\Database;
 
-class MyPDO
+use System\Database\Exceptions\InvalidConfigurationException;
+use System\Database\Interfaces\ConnectionInterface;
+
+class MyPDO implements ConnectionInterface
 {
-    /** @var \PDO PDO */
-    private $dbh;
-    /** @var \PDOStatement */
-    private $stmt;
+    protected \PDO $dbh;
+    private \PDOStatement $stmt;
+
+    /** @var array<int, string|int|bool> */
+    protected array $option = [
+        \PDO::ATTR_PERSISTENT => true,
+        \PDO::ATTR_ERRMODE    => \PDO::ERRMODE_EXCEPTION,
+    ];
 
     /**
      * Connection configuration.
      *
-     * @var array<string, string>
+     * @var array{driver: string, host: ?string, database: ?string, port: ?int, charset: ?string, username: ?string, password: ?string, options: array<int, string|int|bool>}
      */
-    protected $configs;
+    protected array $configs;
 
     /**
      * Query prepare statment;.
@@ -32,18 +39,13 @@ class MyPDO
     protected $logs = [];
 
     /**
-     * @param array<string, string> $configs
+     * @param array<string, string|int|array<int, string|int|bool>|null> $configs
      */
     public function __construct(array $configs)
     {
-        $database_name    = $configs['database_name'];
-        $host             = $configs['host'];
-        $user             = $configs['user'];
-        $pass             = $configs['password'];
-
-        $this->configs = $configs;
-        $dsn           = "mysql:host=$host;dbname=$database_name";
-        $this->useDsn($dsn, $user, $pass);
+        $dsn_config = $this->setConfigs($configs);
+        $dsn        = $this->getDsn($dsn_config);
+        $this->dbh  = $this->createConnection($dsn, $dsn_config, $dsn_config['options']);
     }
 
     /**
@@ -57,6 +59,8 @@ class MyPDO
     }
 
     /**
+     * @deprecated use createConnection instead
+     *
      * @throws \Exception
      */
     protected function useDsn(string $dsn, string $user, string $pass): self
@@ -76,6 +80,94 @@ class MyPDO
     }
 
     /**
+     * @param array<string, string>       $configs
+     * @param array<int, string|int|bool> $options
+     *
+     * @throws \PDOException
+     */
+    protected function createConnection(string $dsn, array $configs, array $options): \PDO
+    {
+        [$username, $password] = [
+            $configs['username'] ?? null, $configs['password'] ?? null,
+        ];
+
+        try {
+            return new \PDO($dsn, $username, $password, $options);
+        } catch (\PDOException $e) {
+            if (true === $this->causedByLostConnection($e)) {
+                return new \PDO($dsn, $username, $password, $options);
+            }
+
+            throw $e;
+        }
+    }
+
+    /**
+     * This code ispired by Laravel Framework.
+     *
+     * @see https://github.com/laravel/framework/blob/9.x/src/Illuminate/Database/DetectsLostConnections.php
+     */
+    protected function causedByLostConnection(\Throwable $e): bool
+    {
+        $errors = [
+            // MySQL/MariaDB
+            'child connection forced to terminate due to client_idle_limit',
+            'SQLSTATE[HY000] [2002] Operation in progress',
+            'Error writing data to the connection',
+            'running with the --read-only option',
+            'Server is in script upgrade mode',
+            'Packets out of order. Expected',
+            'Resource deadlock avoided',
+            'is dead or not enabled',
+            'server has gone away',
+            'Error while sending',
+            'query_wait_timeout',
+            'Lost connection',
+            // PostgreSQL
+            'could not connect to server: Connection refused',
+            'server closed the connection unexpectedly',
+            'connection is no longer usable',
+            'no connection to the server',
+            // SQLite
+            'No such file or directory',
+            'Transaction() on null',
+            // SSL
+            'SQLSTATE[HY000]: General error: 7 SSL SYSCALL error',
+            'SSL connection has been closed unexpectedly',
+            'decryption failed or bad record mac',
+            'SSL: Connection timed out',
+            'SSL: Operation timed out',
+            'SSL: Broken pipe',
+            // Network error
+            'The connection is broken and recovery is not possible',
+            'Physical connection is not usable',
+            'Communication link failure',
+            'No route to host',
+            'reset by peer',
+            // Network timeout
+            'Connection timed out',
+            'Login timeout expired',
+            // General error
+            'SQLSTATE[HY000] [2002] Connection refused',
+            'SQLSTATE[08S01]: Communication link failure',
+            'php_network_getaddresses: getaddrinfo failed',
+            'The client was disconnected by the server because of inactivity',
+            'Temporary failure in name resolution',
+            'could not translate host name',
+        ];
+
+        $message = $e->getMessage();
+
+        foreach ($errors as $error) {
+            if (false !== stripos($message, $error)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
      * Create connaction using static.
      *
      * @param array<string, string> $configs
@@ -90,7 +182,7 @@ class MyPDO
     /**
      * Get connection configuration.
      *
-     * @return array<string, string>
+     * @return array{driver: string, host: ?string, database: ?string, port: ?int, charset: ?string, username: ?string, password: ?string, options: array<int, string|int|bool>}
      */
     public function configs()
     {
@@ -98,7 +190,107 @@ class MyPDO
     }
 
     /**
-     *  mempersiapkan statement pada query.
+     * @param array<string, string|int|array<int, int|bool>|null> $configs
+     *
+     * @return array{driver: string, host: ?string, database: ?string, port: ?int, charset: ?string, username: ?string, password: ?string, options: array<int, string|int|bool>}
+     */
+    protected function setConfigs(array $configs): array
+    {
+        return $this->configs = [
+            'driver'   => $configs['driver'] ?? 'mysql',
+            'host'     => $configs['host'] ?? null,
+            'database' => $configs['database_name'] ?? $configs['database'] ?? null,
+            'port'     => $configs['port'] ?? null,
+            'charset'  => $configs['charset'] ?? null,
+            'username' => $configs['user'] ?? $configs['username'] ?? null,
+            'password' => $configs['password'] ?? null,
+            'options'  => $configs['options'] ?? $this->option,
+        ];
+    }
+
+    /**
+     * @param array{host: string, driver: 'mysql'|'mariadb'|'pgsql'|'sqlite', database: ?string, port: ?int, charset: ?string} $configs
+     */
+    public function getDsn(array $configs): string
+    {
+        return match ($configs['driver']) {
+            'mysql', 'mariadb' => $this->makeMysqlDsn($configs),
+            'pgsql'  => $this->makePgsqlDsn($configs),
+            'sqlite' => $this->makeSqliteDsn($configs),
+        };
+    }
+
+    /**
+     * @param array<string, string|int|array<int, string|bool>> $config
+     *
+     * @throws InvalidConfigurationException
+     */
+    private function makeMysqlDsn(array $config): string
+    {
+        // required
+        if (false === array_key_exists('host', $config)) {
+            throw new InvalidConfigurationException('mysql driver require `host`.');
+        }
+        $dsn['host']    = "host={$config['host']}";
+        $dsn['dbname']  = isset($config['database']) ? "dbname={$config['database']}" : '';
+        $dsn['port']    = 'port=' . ($config['port'] ?? 3306);
+        $dsn['charset'] = 'charset=' . ($config['charset'] ?? 'utf8mb4');
+
+        $build = implode(';', array_filter($dsn, fn (string $item): bool => '' !== $item));
+
+        return "mysql:{$build}";
+    }
+
+    /**
+     * @param array<string, string|int|array<int, string|bool>> $config
+     *
+     * @throws InvalidConfigurationException
+     */
+    private function makePgsqlDsn(array $config): string
+    {
+        // required
+        if (false === array_key_exists('host', $config)) {
+            throw new InvalidConfigurationException('pgsql driver require `host`.');
+        }
+
+        $dsn['host']     = "host={$config['host']}";
+        $dsn['dbname']   = isset($config['database']) ? "dbname={$config['database']}" : '';
+        $dsn['port']     = 'port=' . ($config['port'] ?? 5432);
+        $dsn['encoding'] = 'client_encoding=' . ($config['charset'] ?? 'utf8');
+
+        $build = implode(';', array_filter($dsn, fn (string $item): bool => '' !== $item));
+
+        return "pgsql:{$build}";
+    }
+
+    /**
+     * @param array<string, string|int|array<int, string|bool>> $config
+     *
+     * @throws InvalidConfigurationException
+     */
+    private function makeSqliteDsn(array $config): string
+    {
+        if (false === array_key_exists('database', $config)) {
+            throw new InvalidConfigurationException('sqlite driver require `database`.');
+        }
+        $path = $config['database'];
+
+        if ($path === ':memory:'
+            || str_contains($path, '?mode=memory')
+            || str_contains($path, '&mode=memory')
+        ) {
+            return "sqlite:{$path}";
+        }
+
+        if (false === ($path = realpath($path))) {
+            throw new InvalidConfigurationException('sqlite driver require `database` with absolute path.');
+        }
+
+        return "sqlite:{$path}";
+    }
+
+    /**
+     * {@inheritdoc}
      */
     public function query(string $query): self
     {
@@ -108,13 +300,9 @@ class MyPDO
     }
 
     /**
-     * Menggantikan paramater input dari user dengan sebuah placeholder.
-     *
-     * @param int|string|bool|null $param
-     * @param mixed                $value
-     * @param int|string|bool|null $type
+     * {@inheritdoc}
      */
-    public function bind($param, $value, $type = null): self
+    public function bind(string|int|bool|null $param, mixed $value, string|int|bool|null $type = null): self
     {
         if (is_null($type)) {
             $type = match (true) {
@@ -130,29 +318,21 @@ class MyPDO
     }
 
     /**
-     * Menjalankan atau mengeksekusi query.
-     *
-     * @return bool True if success
-     *
-     * @throws \PDOException
+     * {@inheritdoc}
      */
-    public function execute()
+    public function execute(): bool
     {
         $start    = microtime(true);
         $execute  = $this->stmt->execute();
-        $elapsed  = round((microtime(true) - $start) * 1000, 2);
-
-        $this->addLog($this->query, $elapsed);
+        $this->addLog($this->query, $start, microtime(true));
 
         return $execute;
     }
 
     /**
-     * mengembalikan hasil dari query yang dijalankan berupa array.
-     *
-     * @return mixed[]|false
+     * {@inheritdoc}
      */
-    public function resultset()
+    public function resultset(): array|false
     {
         $this->execute();
 
@@ -160,11 +340,9 @@ class MyPDO
     }
 
     /**
-     * Mengembalikan hasil dari query, ditampilkan hanya satu baris data saja.
-     *
-     * @return mixed
+     * {@inheritdoc}
      */
-    public function single()
+    public function single(): mixed
     {
         $this->execute();
 
@@ -172,48 +350,48 @@ class MyPDO
     }
 
     /**
-     * menampilkan jumlah data yang berhasil di simpan, di ubah maupun dihapus.
-     *
-     * @return int the number of rows
+     * {@inheritdoc}
      */
-    public function rowCount()
+    public function rowCount(): int
     {
         return $this->stmt->rowCount();
     }
 
     /**
-     * id dari data yang terakhir disimpan.
-     *
-     * @return string|false last id
+     * {@inheritdoc}
      */
-    public function lastInsertId()
+    public function lastInsertId(): string|false
     {
         return $this->dbh->lastInsertId();
     }
 
     /**
-     * @return bool Transaction status
+     * {@inheritdoc}
      */
-    public function transaction(callable $callable)
+    public function transaction(callable $callable): bool
     {
-        if (false === $this->beginTransaction()) {
+        try {
+            if (false === $this->beginTransaction()) {
+                return false;
+            }
+
+            $return_call =  call_user_func($callable, $this);
+            if (true !== $return_call) {
+                $this->cancelTransaction();
+
+                return false;
+            }
+
+            return $this->endTransaction();
+        } catch (\Throwable $th) {
+            $this->cancelTransaction();
+
             return false;
         }
-
-        $return_call =  call_user_func($callable);
-        if (false === $return_call) {
-            return $this->cancelTransaction();
-        }
-
-        return $this->endTransaction();
     }
 
     /**
-     * Initiates a transaction.
-     *
-     * @return bool True if success
-     *
-     * @throws \PDOException
+     * {@inheritdoc}
      */
     public function beginTransaction(): bool
     {
@@ -221,11 +399,7 @@ class MyPDO
     }
 
     /**
-     * Commits a transaction.
-     *
-     * @return bool True if success
-     *
-     * @throws \PDOException
+     * {@inheritdoc}
      */
     public function endTransaction(): bool
     {
@@ -233,27 +407,25 @@ class MyPDO
     }
 
     /**
-     * Rolls back a transaction.
-     *
-     * @return bool True if success
-     *
-     * @throws \PDOException
+     * {@inheritdoc}
      */
     public function cancelTransaction(): bool
     {
         return $this->dbh->rollBack();
     }
 
-    protected function addLog(string $query, float $elapsed_time): void
+    protected function addLog(string $query, float $start_time, float $end_time): void
     {
         $this->logs[] = [
-            'query' => $query,
-            'time'  => $elapsed_time,
+            'query'    => $query,
+            'started'  => $start_time,
+            'ended'    => $end_time,
+            'duration' => null,
         ];
     }
 
     /**
-     * Flush logs query.
+     * {@inheritdoc}
      */
     public function flushLogs(): void
     {
@@ -261,12 +433,15 @@ class MyPDO
     }
 
     /**
-     * Get logs query.
-     *
-     * @return array<int, array<string, mixed>>
+     * {@inheritdoc}
      */
     public function getLogs(): array
     {
+        foreach ($this->logs as &$log) {
+            $log['duration'] ??= round(($log['ended'] - $log['started']) * 1000, 2);
+        }
+        unset($log);
+
         return $this->logs;
     }
 }
